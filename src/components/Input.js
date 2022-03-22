5df378e2cf6578b8ef37d0010b6e43a402faef92
tkmoo <thialmoore@yahoo.com>
2022-03-21 23:33:41 -0400

    did most components

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,12 +34,13 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from "react"; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
+  const [inputValue, setInputValue] = useState("");
 
-  const changeInput = evt => {
+  const changeInput = (evt) => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
@@ -48,21 +49,42 @@ export default function Input() {
   };
   const reset = () => {
     /* STEP 5 */
+    <button onClick={() => setInputValue("")}></button>;
   };
 
   const style = {
-    fontSize: '1.5em',
-    marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    fontSize: "1.5em",
+    marginBottom: "0.3em",
+    color: inputValue > 10 ? "royalblue" : "" /* STEP 2 */,
+  };
+
+  const style2 = {
+    fontSize: "1.5em",
+    marginBottom: "0.3em",
+    color: "crimson",
+  };
+
+  const adjustColor = (length) => {
+    if (inputValue + length > 10) {
+      return style2;
+    }
+    setInputValue(inputValue + length);
   };
 
   return (
-    <div className='widget-input container'>
+    <div className="widget-input container">
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id="output" style={style}>
+        {inputValue.map((val) => {
+          val.toUpperCase();
+        })}
+      </div>{" "}
+      {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
-        <button id='resetInput' onClick={reset}>Reset</button>
+        <input id="input" type="text" onChange={changeInput} /> {/* STEP 6 */}
+        <button id="resetInput" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import Counter from './components/Counter';
-import Input from './components/Input';
-import Moods from './components/Moods';
-import Spinner from './components/Spinner';
-import Squares from './components/Squares';
-import Programmers from './components/Programmers';
+import Counter from "./components/Counter";
+import Input from "./components/Input";
+import Moods from "./components/Moods";
+import Spinner from "./components/Spinner";
+import Squares from "./components/Squares";
+import Programmers from "./components/Programmers";
 
 render(
   <>
     <Counter />
     <Moods />
     <Spinner />
-    <Input />
+    <Input value={""} />
     <Squares />
     <Programmers />
-  </>
-  ,
-  document.querySelector('#root')
+  </>,
+  document.querySelector("#root")
 );
